87bd5a2642906394825378bc3bb8789972c3e739
Marcio Rocha <60201245+MarciovsRocha@users.noreply.github.com>
2025-04-09 17:16:48 -0300

    definidos models

--- backend/models/part.model.js
+++ backend/models/part.model.js
@@ -1,7 +1,14 @@
-module.exports = {
-  id: Number,
-  maintenance_id: Number,
-  name: String,
-  brand: String,
-  price: Number
+const db = require('../config/db');
+
+exports.create = async (part) => {
+  const [result] = await db.query(
+      'INSERT INTO maintenance_parts (maintenance_id, part_name, part_code, price, quantity) VALUES (?, ?, ?, ?, ?) ',
+      [part.maintenance_id, part.name, part.code, part.price, part.quantity]
+  );
+  return result.insertId;
+};
+
+exports.getByMaintenance = async (maintenance_id) => {
+  const [result] = await db.query('SELECT * FROM maintenance_parts WHERE maintenance_id = ?', [maintenance_id]);
+  return result;
 };
--- backend/models/service.model.js
+++ backend/models/service.model.js
@@ -1,6 +1,14 @@
-module.exports = {
-  id: Number,
-  maintenance_id: Number,
-  name: String,
-  price: Number
+const db = require('../config/db');
+
+exports.create = async (service) => {
+  const [result] = await db.query(
+      'INSERT INTO maintenance_services (maintenance_id, service_description, cost) VALUES (?, ?, ?) ',
+      [service.maintenance_id, service.description, service.cost]
+  );
+  return result.insertId;
+};
+
+exports.getByMaintenance = async (maintenance_id) => {
+  const [result] = await db.query('SELECT * FROM maintenance_services WHERE maintenance_id = ?', [maintenance_id]);
+  return result;
 };

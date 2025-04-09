1217d87653e5200ce298c2a42e2601d1b9176766
Marcio Rocha <60201245+MarciovsRocha@users.noreply.github.com>
2025-04-09 17:16:38 -0300

    ajustado para que controller faça uso de model

--- backend/controller/service.controller.js
+++ backend/controller/service.controller.js
@@ -1,6 +1,34 @@
-const db = require('../config/db');
+const Service = require('../models/service.model');
+const Maintenance = require('../models/maintenance.model');
 
 exports.getByMaintenance = async (req, res) => {
-  const [rows] = await db.query('SELECT * FROM services WHERE maintenance_id = ?', [req.params.maintenance_id]);
-  res.json(rows);
+  try{
+    const id = req.params.maintenance_id;
+    const maintenance = await Maintenance.getById(id);
+    
+    if (!maintenance){
+      return res.status(404).json({message: 'Service not found'});
+    }
+    
+    const my_maintenances = await Maintenance.getByUserId(req.user.id);
+    if (!my_maintenances){
+      return res.status(404).json({message: 'Service not found'});
+    }
+    
+    let its_mine = false;
+    my_maintenances.forEach(my_maintenance => {
+      its_mine = its_mine || (my_maintenance.id == maintenance.id);
+    });
+    
+    if (! its_mine){
+      return res.status(404).json({message: 'Service not found'});
+    }
+    
+    const services = await Service.getByMaintenance(maintenance.id);
+
+    return res.json(services);
+    
+  } catch(err){
+    return res.status(500).json({message: 'Error Fetching services'});
+  }
 };
